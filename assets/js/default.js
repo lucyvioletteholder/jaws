935cde22fe92b8713a0b3fa2794b25136165e64c
Lucy Holder <lvhs8799@gmail.com>
2020-10-29 11:55:59 +1300

    go to black page

--- assets/js/default.js
+++ assets/js/default.js
@@ -14,14 +14,8 @@ $(function() {
       // $(".jaws-shark--swimming").addClass("jaws-shark--swimming-away");
       attacked = true;
 
-      // // @todo at the end of the attack the screen goes black COULD B WRONG
-      // $(".jaws-shark--attacking").addClass("jaws-blackscreen");
-
     } else {
-      // // *trying to make when screen goes black - click to go to swimming page*
-      // $("jaws-blackscreen".addClass("");
-      // When you click the black (so when attacked is true already)
-      // Then go to swimming page
+      // Then go to black page
       console.log('attacked already');
       window.location = '/black';
     }
